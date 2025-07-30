226a3d7b4fb7db1986b0d0ee561eb3c32e877d99
Pawan Yaduveer <pawankr8107@gmail.com>
2025-07-31 01:31:32 +0530

    Updated certificates

--- src/pages/Certificates.js
+++ src/pages/Certificates.js
@@ -7,16 +7,16 @@ const certificates = [
   {
     id: 1,
     title: 'Full-Stack using MERN',
-    image: 'https://drive.google.com/uc?export=view&id=YOUR_FILE_ID_1',
+    image: 'https://drive.google.com/file/d/19BQETTIcuo29pDokbljuEh-p5ht9c_qY/view',
     description: 'I have successfully completed a comprehensive Full-Stack Web Development training program using the MERN stack (MongoDB, Express.js, React.js, and Node.js) from CipherSchools during January–February 2024. Through this training, I gained hands-on experience in building full-stack web applications, including designing dynamic user interfaces using React, creating RESTful APIs with Express and Node.js, and managing data with MongoDB. .',
-    link: 'https://drive.google.com/file/d/YOUR_FILE_ID_1/view',
+    link: 'https://drive.google.com/file/d/19BQETTIcuo29pDokbljuEh-p5ht9c_qY/view?usp=drive_link',
   },
   {
     id: 2,
     title: 'Data Structures and Algorithms',
     image: 'https://drive.google.com/uc?export=view&id=YOUR_FILE_ID_2',
     description: 'I have completed the Data Structures and Algorithms (C++) course from GeeksforGeeks, where I learned core concepts like arrays, linked lists, trees, graphs, sorting, searching, and dynamic programming. This course enhanced my problem-solving skills and deepened my understanding of algorithmic thinking using C++.Through hands-on coding exercises and problem-solving challenges, I significantly improved my logical thinking and problem-solving skills.',
-    link: 'https://drive.google.com/file/d/YOUR_FILE_ID_2/view',
+    link: 'https://drive.google.com/file/d/11O_QPQnJt2I-77OgfQOLqX6x5JeNSkHO/view?usp=drive_link',
   },
 
 
